04c52b35d1a5857412dae04470da6afdbb3a18f0
jendcruz22 <jendcruz23@gmail.com>
2020-10-20 19:19:16 +0530

    modified: about us layout

--- lib/screens/about_us.dart
+++ lib/screens/about_us.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AboutUs extends StatelessWidget {
@@ -18,217 +19,257 @@ class AboutUs extends StatelessWidget {
         centerTitle: true,
       ),
 
-      /// Body
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 100.0,
-            ),
+        body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
 
-            /// Images
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                // SizedBox(
-                //   width: 40.0,
-                // ),
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/jen.jpg'),
-                  radius: 80.0,
-                ),
-                // SizedBox(
-                //   width: 30.0,
-                // ),
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/dan.jpg'),
-                  radius: 80.0,
-                ),
-              ],
-            ),
+            children: <Widget>[
+              /// Images
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 20, 20, 20),
+                child: Column(
+                  children: [
 
-            Divider(
-              height: 50.0,
-              color: Colors.grey[800],
-            ),
+                    SizedBox(height: 30.0,),
 
-            /// Names
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 50.0,
-                ),
-                SizedBox(
-                  width: 5.0,
-                ),
-                Text(
-                  'Jenny Dcruz',
-                  style: TextStyle(
-                    color: Colors.amber,
-                    letterSpacing: 2.0,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                SizedBox(
-                  width: 35.0,
-                ),
-                Text(
-                  'Daniel Lobo',
-                  style: TextStyle(
-                    color: Colors.amber,
-                    letterSpacing: 2.0,
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
+                    Container(
+                      child: new Stack(
+                        children: <Widget>[
+                          Card(
+                            child: Container(
+                              height: 200.0,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          FractionalTranslation(
+                            translation: Offset(0.0, -0.4),
+                            child: Align(
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage('images/dan.jpg'),
+                                radius: 50.0,
+                              ),
+                              alignment: FractionalOffset(0.5, 0.0),
+                            ),
+                          ),
 
-            /// Class
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  width: 10.0,
-                ),
-                Icon(
-                  Icons.account_circle,
-                  size: 20.0,
-                  color: Colors.grey[800],
-                ),
-                SizedBox(
-                  width: 5.0,
-                ),
-                Text(
-                  'BE CMPN A - 16',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 20.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Icon(
-                  Icons.account_circle,
-                  size: 20.0,
-                  color: Colors.grey[800],
-                ),
-                SizedBox(
-                  width: 5.0,
-                ),
-                Text(
-                  'BE CMPN A - 07',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 20.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
+                          Center(
+                            child:
+                            Column(
+                              children: [
 
-            SizedBox(
-              height: 10.0,
-            ),
+                                SizedBox(height: 70.0,),
 
-            /// PIDs
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: 40.0,
-                ),
-                Icon(
-                  Icons.work,
-                  size: 18.0,
-                  color: Colors.grey[800],
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'PID 172047',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-                SizedBox(
-                  width: 80.0,
-                ),
-                Icon(
-                  Icons.work,
-                  size: 18.0,
-                  color: Colors.grey[800],
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'PID 172047',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 18.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
+                                Text(
+                                  'Daniel Lobo',
+                                  style: TextStyle(
+                                    color: Colors.amber,
+                                    fontSize: 25,
+                                    fontWeight: FontWeight.w900,
+                                    letterSpacing: 3.0
+                                  ),
+                                ),
 
-            SizedBox(
-              height: 10.0,
-            ),
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 120.0,
+                                      height: 40.0,),
 
-            /// Emails
-            Row(
-              children: <Widget>[
-                SizedBox(
-                  width: 30.0,
+                                    Icon(
+                                      Icons.person,
+                                      size: 15.0,
+                                      color: Colors.white,
+                                    ),
+
+                                    SizedBox(width: 5.0,),
+
+                                    Text(
+                                      'BE CMPN A - 07',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 130.0,
+                                      ),
+
+                                    Icon(
+                                      Icons.work,
+                                      size: 15.0,
+                                      color: Colors.white,
+                                    ),
+
+                                    SizedBox(width: 5.0,),
+
+                                    Text(
+                                      'PID: 172047',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 90.0,
+                                      height: 40.0,),
+
+                                    Icon(
+                                      Icons.email,
+                                      size: 15.0,
+                                      color: Colors.white,
+                                    ),
+
+                                    SizedBox(width: 5.0,),
+
+                                    Text(
+                                      'danlobo1999@gmail.com',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+
+                    SizedBox(height: 50.0,),
+                    Container(
+                      child: new Stack(
+                        children: <Widget>[
+                          Card(
+                            child: Container(
+                              height: 200.0,
+                              color: Colors.blue,
+                            ),
+                          ),
+                          FractionalTranslation(
+                            translation: Offset(0.0, -0.4),
+                            child: Align(
+                              child: CircleAvatar(
+                                backgroundImage: AssetImage('images/jen.jpg'),
+                                radius: 50.0,
+                              ),
+                              alignment: FractionalOffset(0.5, 0.0),
+                            ),
+                          ),
+
+                          Center(
+                            child:
+                            Column(
+                              children: [
+
+                                SizedBox(height: 70.0,),
+
+                                Text(
+                                  'Jenny Dcruz',
+                                  style: TextStyle(
+                                      color: Colors.amber,
+                                      fontSize: 25,
+                                      fontWeight: FontWeight.w900,
+                                      letterSpacing: 3.0
+                                  ),
+                                ),
+
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 120.0,
+                                      height: 40.0,),
+
+                                    Icon(
+                                      Icons.person,
+                                      size: 15.0,
+                                      color: Colors.white,
+                                    ),
+
+                                    SizedBox(width: 5.0,),
+
+                                    Text(
+                                      'BE CMPN A -16',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 130.0,
+                                    ),
+
+                                    Icon(
+                                      Icons.work,
+                                      size: 15.0,
+                                      color: Colors.white,
+                                    ),
+
+                                    SizedBox(width: 5.0,),
+
+                                    Text(
+                                      'PID: 172047',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Row(
+                                  children: [
+                                    SizedBox(
+                                      width: 100.0,
+                                      height: 40.0,),
+
+                                    Icon(
+                                      Icons.email,
+                                      size: 15.0,
+                                      color: Colors.white,
+                                    ),
+
+                                    SizedBox(width: 5.0,),
+
+                                    Text(
+                                      'jendcruz23@gmail.com',
+                                      style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15.0,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                              ],
+                            ),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                Icon(
-                  Icons.email,
-                  size: 12.0,
-                  color: Colors.grey[800],
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'jendcruz23@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 12.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-                SizedBox(
-                  width: 20.0,
-                ),
-                Icon(
-                  Icons.email,
-                  size: 12.0,
-                  color: Colors.grey[800],
-                ),
-                SizedBox(
-                  width: 10.0,
-                ),
-                Text(
-                  'danlobo1999@gmail.com',
-                  style: TextStyle(
-                    color: Colors.grey[800],
-                    fontSize: 12.0,
-                    letterSpacing: 1.0,
-                  ),
-                ),
-              ],
-            ),
-          ],
+              ),
+            ],
         ),
       ),
     );
